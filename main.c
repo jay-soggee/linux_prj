4ebf70589d9b027bdb32061ddc268f21c0ffffb3
Bear_Jay <jaesuki98@gmail.com>
2022-12-17 22:23:53 +0900

    .

--- main.c
+++ main.c
@@ -26,9 +26,13 @@ Pitime NOW() {
 int isTimePassed_us(Pitime* ref, int time_us) {
     // 1s = 1,000,000us, 1us = 1,000ns
     int time_sec  = time_us / 1000000;
-    int time_nsec = (time_us % 1000000) * 1000;
+    int time_nsec = (time_us - time_sec) * 1000;
     int passed_sec = ref->tv_sec + time_sec;
     int passed_nsec = ref->tv_nsec + time_nsec;
+    if (passed_nsec > 1000000000) { // control overflow
+        passed_sec++;
+        passed_nsec -= 1000000000;
+    }
     clock_gettime(CLOCK_REALTIME, &gettime_now);
     return (gettime_now.tv_sec < passed_sec) ? 0 : (gettime_now.tv_nsec < passed_nsec) ? 0 : 1;
 }
